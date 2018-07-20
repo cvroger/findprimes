18b4f690c20236d11395ffe38386ed3d6e7f8cbd
cvroger <roger@chillimktg.com>
2018-07-20 23:25:24 +0800

    It's a quick and dirty version
    It's a quick and dirty version. The adding of "2" in the result is like a joke...

---
+++ findprimes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+void findprimes(int, int);
+
+int main(){
+    int src1, src2;
+    printf("Input the starting integer (0-2,147,483,647): ");
+    scanf("%d", &src1);
+    printf("Input the ending integer (0-2,147,483,647): ");
+    scanf("%d", &src2);
+    if ((src1<0 || src1>2147483647)||(src2<0 || src2>2147483647)||(src1>src2)){
+        printf("I appreciate your sense of humor. Bye!");
+        return 0;
+    }
+    findprimes(src1, src2);
+    return 0;
+}
+
+void findprimes(int x, int y){
+    int testee=0, counter=0;
+    for (testee=x; testee<=y; testee++){
+        int max = (int)sqrt(testee)+1;
+        int i;
+        int factor = 0;
+        for (i=2; i<=max; i++){
+            if (testee % i == 0){
+                factor++;
+            }
+        }
+        if (factor == 0 || testee ==2){
+            printf("Found prime number: %d\n", testee );
+            counter++;
+        }
+    }
+    printf("#Primes Found in Range: %d\n", counter);
+}
