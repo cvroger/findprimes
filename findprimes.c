da2b9f6c7109336ed807330353e5d3dbe5d20048
cvroger <roger@chillimktg.com>
2018-07-24 14:35:04 +0800

    print_to_file
    now added a print to file function

--- findprimes.c
+++ findprimes.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 void findprimes(int, int);
 
-int main(){
+int main()
+{
     int src1, src2;
     printf("Input the starting integer (0-2,147,483,647): ");
     scanf("%d", &src1);
     printf("Input the ending integer (0-2,147,483,647): ");
     scanf("%d", &src2);
-    if ((src1<0 || src1>2147483647)||(src2<0 || src2>2147483647)||(src1>src2)){
+    if ((src1<0 || src1>2147483647)||(src2<0 || src2>2147483647)||(src1>src2))
+    {
         printf("I appreciate your sense of humor. Bye!");
         return 0;
     }
     findprimes(src1, src2);
     return 0;
 }
-
-void findprimes(int x, int y){
+void findprimes(int x, int y)
+{
     int testee=0, counter=0;
-    for (testee=x; testee<=y; testee++){
+    char filename[30];
+    FILE * outputFile;
+    sprintf(filename, "prime_%d_%d.txt", x, y);
+    outputFile = fopen(filename, "w");
+    fprintf(outputFile, "Prime Numbers from %d to %d:\n", x, y);
+    for (testee=x; testee<=y; testee++)
+    {
         int max = (int)sqrt(testee)+1;
         int i;
         int factor = 0;
-        for (i=2; i<=max; i++){
-            if (testee % i == 0){
+        for (i=2; i<=max; i++)
+        {
+            if (testee % i == 0)
+            {
                 factor++;
             }
         }
-        if (factor == 0 || testee ==2){
+        if (factor == 0 || testee ==2)
+        {
             printf("Found prime number: %d\n", testee );
+            fprintf(outputFile, "Found prime number: %d\n", testee );
             counter++;
         }
     }
     printf("#Primes Found in Range: %d\n", counter);
+    fprintf(outputFile, "#Primes Found in Range: %d\n", counter);
+    fclose(outputFile);
 }
